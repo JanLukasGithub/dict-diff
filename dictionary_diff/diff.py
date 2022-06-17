61b1cdd2800d59e37fd0fd965f04e0c25065ff9e
JanLukasGithub <jan@garloff.de>
2022-06-17 15:09:21 +0200

    Add diff() function operating on any type

--- dictionary_diff/diff.py
+++ dictionary_diff/diff.py
@@ -23,21 +23,31 @@ def equivalent(element1, element2) -> bool:
 
     - The types of element1 and element2 are the same
     - - For lists: if the lists are equal regardless of order
-      - For dicts: if all members are equivalent
+      - For dicts: if all key-value pairs are equivalent
       - For everything else: if `element1 == element2`
 
     :rtype: bool
     """
-    import dictionary_diff.dict_diff as dict_diff
-    import dictionary_diff.list_diff as list_diff
-
     if type(element1) is not type(element2):
         return False
 
     if isinstance(element1, list):
-        return list_diff.list_unordered_equal(element1, element2)
+        from dictionary_diff.list_diff import list_equivalent
+        return list_equivalent(element1, element2)
 
     if isinstance(element1, dict):
-        return dict_diff.dict_equivalent(element1, element2)
+        from dictionary_diff.dict_diff import dict_equivalent
+        return dict_equivalent(element1, element2)
 
     return element1 == element2
+
+def diff(orig, other):
+    if isinstance(orig, dict) and isinstance(other, dict):
+        from dictionary_diff.dict_diff import dict_diff
+        return dict_diff(orig, other)
+
+    if isinstance(orig, list) and isinstance(other, list):
+        from dictionary_diff.list_diff import list_diff
+        return list_diff(orig, other)
+    
+    return other
