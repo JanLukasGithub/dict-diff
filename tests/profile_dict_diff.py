d2e0e47a4411efb7f573f383a3b0b9ec1058700b
JanLukasGithub <jan@garloff.de>
2022-06-25 22:45:48 +0200

    Use diff.diff() for profiling

--- tests/profile_dict_diff.py
+++ tests/profile_dict_diff.py
@@ -1,11 +1,11 @@
-from dictionary_diff.dict_diff import *
+from dictionary_diff import diff
 from tests.test_dict_diff import test_cases
 import cProfile
 
 def run_n_times(n=100):
     for i in range(n):
         for test in test_cases:
-            dict_diff(test["orig"], test["new"])
+            diff.diff(test["orig"], test["new"])
 
 if __name__ == "__main__":
     cProfile.run('run_n_times(n=10000)')
