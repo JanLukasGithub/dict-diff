80d2b426ae9af99bcd37ff86c49b981c1bb603d9
JanLukasGithub <jan@garloff.de>
2022-06-25 16:58:51 +0200

    Implement list_diff

--- dictionary_diff/list_diff.py
+++ dictionary_diff/list_diff.py
@@ -3,9 +3,11 @@
 """
 
 import dictionary_diff.diff as diff
+import dictionary_diff.dict_diff as dict_diff
+from dictionary_diff.diff import _Remove
 
 
-def list_unordered_equal(list1: list, list2: list) -> bool:
+def list_equivalent(list1: list, list2: list) -> bool:
     """
     :return: True if and only if each value in list1 has exactly one
      :func:`~dict_diff.dict_diff.equivalent` value in list2
@@ -24,14 +26,95 @@ def list_unordered_equal(list1: list, list2: list) -> bool:
 
     return not list2_copy
 
-def add_different_list(orig: list, other: list, equivalent_func=diff.equivalent) -> list:
+def list_diff(orig: list, other: list, equivalent_func=diff.equivalent) -> list:
     """
-    :return: the list, where all other's values not :func:`~dict_diff.dict_diff.equivalent`
-     to ones in orig are present. If the value dict or list, only non
-     :func:`~dict_diff.dict_diff.equivalent` parts of that are present
-    :rtype: list
+    :param orig: The original list
+    :param other: The list the diff is taken of
+    :param equivalent_func: This method is used for determining if two elements
+     (of any types) are equivalent,
+     defaults to :func:`~dict_diff.dict_diff.equivalent`
 
-    This is faster than :func:`~dict_diff.dict_diff.remove_equivalent_list`,
-     if the difference between the lists is small
+    :return: The diff, so that :func:`apply_diff(orig, diff) <dict_diff.dict_diff.apply_diff>`
+     returns something :func:`~dict_diff.dict_diff.equivalent` to other
+    :rtype: list
+    """
+    difference = []
+
+    added = find_added(orig, other, equivalent_func)
+    removed = find_removed(orig, other, equivalent_func)
+
+    for item_added in added.copy():
+        for item_removed in removed:
+            if isinstance(item_added, dict) and isinstance(item_removed, dict):
+                difference.append(dict_diff.dict_diff(item_removed, item_added))
+                removed.remove(item_removed)
+                added.remove(item_added)
+                break
+            elif isinstance(item_added, list) and isinstance(item_removed, list):
+                difference.append(list_diff(item_removed, item_added))
+                removed.remove(item_removed)
+                added.remove(item_added)
+                break
+
+    for item in added:
+        difference.append(item)
+
+    for item in removed:
+        difference.append(_Remove(item))
+
+    return difference
+
+def find_equivalent(orig: list, other: list, equivalent_func=diff.equivalent) -> list:
+    """
+    :return: a list of values v that are :func:`~dictionary_diff.diff.equivalent` in orig and other,
+     such that v is a subset of orig and other
+    :rtype: list
+    """
+    found = []
+    orig_copy = orig.copy()
+
+    for item_other in other:
+        for item_orig in orig_copy:
+            if equivalent_func(item_other, item_orig):
+                found.append(item_orig)
+                orig_copy.remove(item_orig)
+                break
+
+    return found
+
+def find_added(orig: list, other: list, equivalent_func=diff.equivalent) -> list:
+    """
+    :return: a list of values v that are in other but not orig,
+     such that v is a subset of other's values
+    :rtype: list
+    """
+    return find_removed(orig=other, other=orig, equivalent_func=equivalent_func)
+
+def find_removed(orig: list, other: list, equivalent_func=diff.equivalent) -> list:
+    """
+    :return: a list of values v that are in orig but not other, such that v is a subset of orig's values
+    :rtype: list
+    """
+    orig_copy = orig.copy()
+
+    for item_other in other:
+        for item_orig in orig_copy:
+            if equivalent_func(item_other, item_orig):
+                orig_copy.remove(item_orig)
+                break
+
+    return orig_copy
+
+def apply_diff(orig: list, other: list) -> list:
+    """
+    Applies the diff to orig
+
+    :param orig: The original list
+    :param diff: The diff to apply
+
+    :return: a list, so that
+     :func:`apply_diff(something, dict_diff(something, other)) <dict_diff.dict_diff.apply_diff>`
+     is :func:`~dict_diff.dict_diff.equivalent` to other
+    :rtype: list
     """
     pass
